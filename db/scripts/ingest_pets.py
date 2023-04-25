99cda75818f5f4521ad370c4742c4be9162368f8
Forest Belton <65484+forestbelton@users.noreply.github.com>
2023-04-25 05:10:33 -0400

    Fix pet ingestion script

--- db/scripts/ingest_pets.py
+++ db/scripts/ingest_pets.py
@@ -1,52 +1,18 @@
 import csv
 import sqlite3
 
-db = sqlite3.connect("pets.sqlite3")
-db.executescript(
-    """
-    CREATE TABLE IF NOT EXISTS pet_skills(
-        id INTEGER PRIMARY KEY,
-        created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        name TEXT NOT NULL UNIQUE,
-        type TEXT NOT NULL,
-        range REAL,
-        time REAL,
-        cooldown REAL,
-        description TEXT NOT NULL
-    );
+db = sqlite3.connect("romdb.sqlite3")
 
-    CREATE TABLE IF NOT EXISTS pets(
-        id INTEGER PRIMARY KEY,
-        created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        name TEXT NOT NULL UNIQUE,
-        type TEXT NOT NULL,
-        skill1 INTEGER NOT NULL REFERENCES pet_skills(id),
-        skill2 INTEGER NOT NULL REFERENCES pet_skills(id),
-        skill3 INTEGER NOT NULL REFERENCES pet_skills(id),
-        skill4 INTEGER NOT NULL REFERENCES pet_skills(id),
-        skill5 INTEGER NOT NULL REFERENCES pet_skills(id),
-        hatch_exp INTEGER NOT NULL,
-        hatch_stat INTEGER,
-        hatch_stat_amount INTEGER
-    );
-
-    CREATE TABLE IF NOT EXISTS pet_catch_items(
-        id INTEGER PRIMARY KEY,
-        created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        pet_id INTEGER NOT NULL REFERENCES pets(id),
-        item_name TEXT NOT NULL,
-        unit_price_shells INTEGER,
-        quantity INTEGER NOT NULL
-    );
-    """
-)
 
 def get_skill_id_by_name(name: str) -> int:
     cursor = db.cursor()
     cursor.execute("SELECT id FROM pet_skills WHERE name = ?", (name,))
     row = cursor.fetchone()
     if row is None:
-        cursor.execute("INSERT INTO pet_skills (name, type, description) VALUES (?, '', '')", (name,))
+        cursor.execute(
+            "INSERT INTO pet_skills (name, type, description) VALUES (?, '', '')",
+            (name,),
+        )
         cursor.execute("SELECT id FROM pet_skills WHERE name = ?", (name,))
         row = cursor.fetchone()
     cursor.close()
@@ -67,31 +33,25 @@ def get_skill_id_by_name(name: str) -> int:
 #
 #     Known affected pets: Mini Gryphon
 def ingest_pets_csv() -> None:
-    print("=== pets === ")
     cursor = db.cursor()
-    with open("pets.csv", "r") as f:
+    with open("csv/pets.csv", "r") as f:
         r = csv.DictReader(f)
         for row in r:
-            print(row)
-            for field in ["hatch_exp", "hatch_stat_amount"]:
-                if row[field] != "":
-                    row[field] = int(row[field])
-                else:
-                    row[field] = None
             for field in ["skill1", "skill2", "skill3", "skill4", "skill5"]:
                 row[field] = get_skill_id_by_name(row[field])
-            print(row)
-            cursor.execute("INSERT INTO pets (name, type, skill1, skill2, skill3, skill4, skill5, hatch_exp, hatch_stat, hatch_stat_amount) VALUES (:name, :type, :skill1, :skill2, :skill3, :skill4, :skill5, :hatch_exp, :hatch_stat, :hatch_stat_amount)", row)
+            cursor.execute(
+                "INSERT INTO pets (name, type, skill1, skill2, skill3, skill4, skill5, hatch_exp) VALUES (:name, :type, :skill1, :skill2, :skill3, :skill4, :skill5, :hatch_exp)",
+                row,
+            )
             db.commit()
     cursor.close()
 
+
 def ingest_pet_skills_csv() -> None:
-    print("=== pet_skills ===")
     cursor = db.cursor()
-    with open("pet_skills.csv", "r") as f:
+    with open("csv/pet_skills.csv", "r") as f:
         r = csv.DictReader(f)
         for row in r:
-            print(row)
             get_skill_id_by_name(row["name"])
             assert row["type"] in {"ACTIVE", "PASSIVE"}
             if row["type"] == "ACTIVE":
@@ -108,18 +68,19 @@ def ingest_pet_skills_csv() -> None:
                 row["time"] = None
                 assert row["cooldown"] == ""
                 row["cooldown"] = None
-            cursor.execute("UPDATE pet_skills SET type = :type, range = :range, time = :time, cooldown = :cooldown, description = :description WHERE name = :name", row)
+            cursor.execute(
+                "UPDATE pet_skills SET type = :type, range = :range, time = :time, cooldown = :cooldown, description = :description WHERE name = :name",
+                row,
+            )
             db.commit()
     cursor.close()
 
 
 def ingest_pet_catch_items_csv() -> None:
-    print("=== pet_catch_items ===")
     cursor = db.cursor()
-    with open("pet_tame_items.csv", "r") as f:
+    with open("csv/pet_tame_items.csv", "r") as f:
         r = csv.DictReader(f)
         for row in r:
-            print(row)
             cursor.execute(
                 """
                 INSERT INTO pet_catch_items (
@@ -134,7 +95,7 @@ def ingest_pet_catch_items_csv() -> None:
                     :quantity
                 )
                 """,
-                row
+                row,
             )
             db.commit()
     cursor.close()
@@ -145,6 +106,6 @@ def main() -> None:
     ingest_pet_skills_csv()
     ingest_pet_catch_items_csv()
 
+
 if __name__ == "__main__":
     main()
-
