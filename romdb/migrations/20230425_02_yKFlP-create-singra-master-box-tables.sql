552f19397ab5c7bc1f97a791282267578c43fb61
Forest Belton <65484+forestbelton@users.noreply.github.com>
2023-05-08 04:08:48 -0400

    Add missing "IF NOT EXISTS" to migrations

--- romdb/migrations/20230425_02_yKFlP-create-singra-master-box-tables.sql
+++ romdb/migrations/20230425_02_yKFlP-create-singra-master-box-tables.sql
@@ -1,13 +1,13 @@
 -- Create Singra Master Box tables
 -- depends: 20230425_01_gDK3c-create-pet-tables
-CREATE TABLE singra_stories(
+CREATE TABLE IF NOT EXISTS singra_stories(
     id INTEGER PRIMARY KEY,
     map_name TEXT NOT NULL,
     story_name TEXT NOT NULL,
     UNIQUE (map_name, story_name)
 );
 
-CREATE TABLE singra_story_quests(
+CREATE IF NOT EXISTS TABLE singra_story_quests(
     id INTEGER PRIMARY KEY,
     story_id INTEGER NOT NULL REFERENCES singra_stories(id),
     quest_number INTEGER NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE singra_story_quests(
     CHECK (quest_number > 0)
 );
 
-CREATE TABLE singra_story_rewards(
+CREATE IF NOT EXISTS TABLE singra_story_rewards(
     id INTEGER PRIMARY KEY,
     story_id INTEGER NOT NULL REFERENCES singra_stories(id),
     item_name TEXT NOT NULL,
